ab6469d09e3aa833133f46657b45c531ed61b23d
zhangenter <83463386@qq.com>
2019-05-02 21:55:31 +0800

    set capture must be utf-8

--- main.py
+++ main.py
@@ -67,8 +67,7 @@ class Main():
         self.image = pygame.transform.scale(self.image, (self.image_width, image_height))
 
     def reset_display_text(self):
-        caption = LanguageLib.instance().get_text('tetris')
-        screencaption = pygame.display.set_caption(caption)
+        screencaption = pygame.display.set_caption(LanguageLib.instance().get_text('tetris'))
         for i in range(len(self.btn_group.btn_list)):
             if i >= len(self.btn_text_arr): break
             self.btn_group.btn_list[i].text = LanguageLib.instance().get_text(self.btn_text_arr[i])
